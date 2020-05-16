f261765191b4b1bcb035136184fc379be780c538
amirisback <faisalamircs@gmail.com>
2020-05-17 02:16:01 +0700

    add: initialize kick-start project file

---
+++ frogothecocktaildbapi/src/main/java/com/frogobox/frogothecocktaildbapi/data/source/CocktailApiCallback.kt
@@ -0,0 +1,18 @@
+package com.frogobox.frogothecocktaildbapi.data.source
+
+/*
+ * Created by Faisal Amir on 17/05/2020
+ * TheCocktailDbApi Source Code
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2020 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+
+abstract class CocktailApiCallback {
+
+}
